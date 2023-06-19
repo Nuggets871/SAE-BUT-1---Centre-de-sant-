eaaacebd1dfd692e66423a89875c0ccd678eea19
HOANG JIUN WEN p2200944 <jiun-wen.hoang@etu.univ-lyon1.fr>
2023-06-19 05:14:53 +0000

    Replace Sommet.java

--- Sommet.java
+++ Sommet.java
@@ -2,21 +2,25 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package sae;
+package modele;
 
-import java.util.List;
 
 /**
  *
  * @author chris
  */
-class Sommet {
-    private String nom;
-    private String type;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sommet {
+    private final String nom;         // Nom du sommet
+    private final String type;        // Type du sommet
+    private final List<Lien> liens;   // Liste des liens associés au sommet
 
     public Sommet(String nom, String type) {
         this.nom = nom;
         this.type = type;
+        this.liens = new ArrayList<>();  // Initialisation de la liste des liens
     }
 
     public String getNom() {
@@ -26,5 +30,12 @@ class Sommet {
     public String getType() {
         return type;
     }
-}
 
+    public void ajouterLien(Lien lien) {
+        liens.add(lien);  // Ajout d'un lien à la liste des liens
+    }
+
+    public List<Lien> getLiens() {
+        return liens;  // Renvoie la liste des liens
+    }
+}
